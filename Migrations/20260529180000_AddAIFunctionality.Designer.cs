3f36c3a351735dda479cc8cf75588d01a24f4379
duongminh-204 <minhduongqh0311@gmail.com>
2026-05-29 23:31:27 +0700

    Sửa File Snapshort và Designer để Update databasse

--- Migrations/20260529180000_AddAIFunctionality.Designer.cs
+++ Migrations/20260529180000_AddAIFunctionality.Designer.cs
@@ -4,6 +4,7 @@ using Bloomy.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BloomyBE.Migrations
 {
     [DbContext(typeof(BloomyDbContext))]
-    partial class BloomyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260529180000_AddAIFunctionality")]
+    partial class AddAIFunctionality
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -1128,3 +1130,4 @@ namespace BloomyBE.Migrations
         }
     }
 }
+
